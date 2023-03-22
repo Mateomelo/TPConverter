27402f8bfc83d4993d278e80eb8a94d0ce2e6562
mateomelo <mateo.melo@outlook.fr>
2023-03-22 12:07:19 +0100

    tp2

--- app/src/main/java/com/example/tpconverter/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/tpconverter/ui/home/HomeFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tpconverter.R
+import com.example.tpconverter.adapter.RateAdapter
 import com.example.tpconverter.databinding.FragmentHomeBinding
+import com.example.tpconverter.model.Rate
 
 class HomeFragment : Fragment() {
 
@@ -28,15 +32,31 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        displayRates()
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun getRates(): MutableList<Rate> {
+        val list = mutableListOf<Rate>()
+        for (i in 1..50) {
+            list.add(Rate(1, "EUR", "Euro", 1.0, "eur"))
+            list.add(Rate(2, "USD", "United States Dollar", 1.1, "usd"))
+        }
+        return list
+    }
+
+    private fun displayRates() {
+        val rateList: RecyclerView = binding.rateRecyclerView
+        val rates = getRates()
+        val adapter = RateAdapter(requireContext(), rates)
+        rateList.adapter = adapter
+        rateList.layoutManager = LinearLayoutManager(requireContext())
+
     }
 }
